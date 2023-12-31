ddd9d0559b65d538f85fdb869fb90d25208e5882
Freemasoid <freemasoid@icloud.com>
2023-12-31 01:33:10 +0100

    commit

--- populate.js
+++ populate.js
@@ -1,11 +1,12 @@
 import "dotenv/config";
-import mockData from "./MOCK_DATA.js";
+import { mockData } from "./MOCK_DATA.js";
 import Product from "./models/Product.js";
-import connectDB from "./db/connect.js";
+import { connectDB } from "./db/connect.js";
 
 const start = async () => {
   try {
     await connectDB(process.env.MONGO_URI);
+    await Product.deleteMany();
     await Product.create(mockData);
     console.log("data successfully uploaded");
     process.exit(0);
