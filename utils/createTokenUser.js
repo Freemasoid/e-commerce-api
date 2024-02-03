5f081b755547fd26acf55da3f4939c760cd66ac6
Freemasoid <freemasoid@icloud.com>
2024-02-03 22:02:27 +0100

    update user

--- utils/createTokenUser.js
+++ utils/createTokenUser.js
@@ -1,5 +1,12 @@
 const createTokenUser = (user) => {
-  return { name: user.name, userId: user._id, role: user.role };
+  return {
+    name: user.name,
+    lastName: user.lastName,
+    userId: user._id,
+    role: user.role,
+    location: user.location,
+    email: user.email,
+  };
 };
 
 export default createTokenUser;
